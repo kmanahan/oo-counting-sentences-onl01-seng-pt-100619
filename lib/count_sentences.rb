0163cba6426c72c9dba7c59c261aeafe90cc8a73
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-07 16:55:11 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-   puts self.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/).count
+   self.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/).count
   end
 end
